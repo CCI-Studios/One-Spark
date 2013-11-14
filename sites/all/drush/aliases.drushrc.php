dccf46bec078b81414814b4c2462a4e29df16ae6
Chris Morris <cmorris@ccistudios.com>
2013-11-14 14:16:41 -0500

    Fix wrapping text around images in blocks

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -11,13 +11,12 @@ $aliases['dev'] = array(
 );
 
 
-$aliases['staging'] = array(
-	'uri'=> 'onespark.ccistaging.com',
-	'root' => '/home/staging/subdomains/onespark/public_html',
-	'remote-host'=> 'host.ccistudios.com',
-	'remote-user'=> 'staging',
+$aliases['live'] = array(
+	'uri'=> 'live.one-spark.ca',
+	'root' => '/home/onespark/subdomains/live/public_html',
+	'remote-host'=> 'host.lambtonshield.com',
+	'remote-user'=> 'onespark',
 	'path-aliases'=> array(
 		'%files'=> 'sites/default/files',
-	),
-	'ssh-options'=> '-p 37241'
+	)
 );
