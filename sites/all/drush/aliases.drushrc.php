064caf0dc08e3e3bd3038ea165cbabd311ac5943
Chris Morris <cmorris@ccistudios.com>
2014-10-29 16:57:10 -0400

    Updated drush aliases

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -3,7 +3,7 @@
 $aliases['dev'] = array(
 	'uri'=> 'dev.one-spark.ca',
 	'root' => '/home/onespark/subdomains/dev/public_html',
-	'remote-host'=> 'host.lambtonshield.com',
+	'remote-host'=> 'host.cciserver2.com',
 	'remote-user'=> 'onespark',
 	'path-aliases'=> array(
 		'%files'=> 'sites/default/files',
@@ -14,7 +14,7 @@ $aliases['dev'] = array(
 $aliases['live'] = array(
 	'uri'=> 'live.one-spark.ca',
 	'root' => '/home/onespark/subdomains/live/public_html',
-	'remote-host'=> 'host.lambtonshield.com',
+	'remote-host'=> 'host.cciserver2.com',
 	'remote-user'=> 'onespark',
 	'path-aliases'=> array(
 		'%files'=> 'sites/default/files',
