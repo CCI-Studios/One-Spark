bb94136be44f839554b341370c7172e8e46a0959
Chris Morris <cmorris@ccistudios.com>
2017-11-21 11:37:38 -0500

    add google tag manager code

--- sites/all/themes/onespark_custom/templates/html.tpl.php
+++ sites/all/themes/onespark_custom/templates/html.tpl.php
@@ -46,6 +46,14 @@
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P9JDJXW');</script>
+<!-- End Google Tag Manager -->
+
   <?php print $head; ?>
   <meta name="viewport" content="width=device-width" id="meta-viewport" />
   <meta name="description" content="One Spark's vision is a world where no woman faces financial barriers to living a life free of violence.">
@@ -56,6 +64,11 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P9JDJXW"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
